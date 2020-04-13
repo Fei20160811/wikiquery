351bc120bf3fc1aea7e0e5f80020ec5213eda073
Fei20160811 <paradox0912@gmail.com>
2020-04-14 00:18:03 +0800

    串接wiki api

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def index(request):
+    try:
+        
+        S = requests.Session()
+        
+        URL = "https://zh.wikipedia.org/w/api.php"
+        txtQuery = request.GET['txtQuery']
+        
+        PARAMS = {
+            "action": "parse",
+            "page": txtQuery,
+            "prop": "text",
+            "format": "json"
+        }
+        
+        response = S.get(url=URL, params=PARAMS)
+        wikiData = response.json()
+        '''wikiFile = open('wikiFile', 'rt')
+        wikiData = wikiFile.read()
+        wikiFile.close()'''
+               
+    except:
+        wikiData = ""    
+    
+    return render(request, 'index.html', locals())
